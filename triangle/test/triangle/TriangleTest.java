5898fcb381a689dfe81bad278931b3e4b9ef206f
Jucong He <juconghe@1x-nat-vl932-172-30-231-21.wireless.umass.edu>
2017-10-19 11:15:42 -0400

    conditional test done --jucong

--- triangle/test/triangle/TriangleTest.java
+++ triangle/test/triangle/TriangleTest.java
@@ -11,13 +11,6 @@ import static triangle.Triangle.Type.*;
  */
 public class TriangleTest {
 
-    @Test
-    public void test1() {
-        Type actual = Triangle.classify(10, 10, 10);
-        Type expected = EQUILATERAL;
-        assertEquals(actual, expected);
-    }
-
     @Test //Jucong test for INVALID
     public void test_invalid() {
       Triangle t = new Triangle();
@@ -26,17 +19,59 @@ public class TriangleTest {
       assertEquals(actual, expected);
     }
 
-    @Test //Jucong test for EQUILATERAL
-    public void test_equilateral() {
-      Type actual = Triangle.classify(1,1,1);
-      Type expected = EQUILATERAL;
-      assertEquals(actual,expected);
-    }
-
     @Test //Jucong test for INVALID not 0/-1 number
     public void test_invalid_2() {
       Type actual = Triangle.classify(1,2,4);
       Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for INVALID
+    public void test_invalid_3() {
+      Type actual = Triangle.classify(2,-1,3);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test  //Jucong test for INVALID
+    public void test_invalid_4() {
+      Type actual = Triangle.classify(2,3,-1);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for INVALID not 0/-1 number
+    public void test_invalid_5() {
+      Type actual = Triangle.classify(1,5,4);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for INVALID not 0/-1 number
+    public void test_invalid_6() {
+      Type actual = Triangle.classify(5,1,4);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for INVALID not 0/-1 number
+    public void test_invalid_7() {
+      Type actual = Triangle.classify(2,100,2);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for INVALID not 0/-1 number
+    public void test_invalid_8() {
+      Type actual = Triangle.classify(100,2,2);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for EQUILATERAL
+    public void test_equilateral() {
+      Type actual = Triangle.classify(1,1,1);
+      Type expected = EQUILATERAL;
       assertEquals(actual,expected);
     }
 
@@ -74,4 +109,5 @@ public class TriangleTest {
       Type expected = INVALID;
       assertEquals(actual,expected);
     }
+
 }
