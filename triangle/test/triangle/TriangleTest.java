6e76668c28b14e0f4e236fc9c38c89f3c3b570ca
Jucong He <juconghe@1x-nat-vl932-172-30-231-21.wireless.umass.edu>
2017-10-19 11:00:35 -0400

    statement coverage done --Jucong

--- triangle/test/triangle/TriangleTest.java
+++ triangle/test/triangle/TriangleTest.java
@@ -17,4 +17,61 @@ public class TriangleTest {
         Type expected = EQUILATERAL;
         assertEquals(actual, expected);
     }
+
+    @Test //Jucong test for INVALID
+    public void test_invalid() {
+      Triangle t = new Triangle();
+      Type actual = Triangle.classify(-1,2,3);
+      Type expected = INVALID;
+      assertEquals(actual, expected);
+    }
+
+    @Test //Jucong test for EQUILATERAL
+    public void test_equilateral() {
+      Type actual = Triangle.classify(1,1,1);
+      Type expected = EQUILATERAL;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for INVALID not 0/-1 number
+    public void test_invalid_2() {
+      Type actual = Triangle.classify(1,2,4);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
+
+    @Test //Jucong test for SCALENE
+    public void test_scalene() {
+      Type actual = Triangle.classify(2,3,4);
+      Type expected = SCALENE;
+      assertEquals(actual,expected);
+    }
+
+    @Test // Jucong test for ISOSCELES
+    public void test_isosceles_1() {
+      Type actual = Triangle.classify(1,2,2);
+      Type expected = ISOSCELES;
+      assertEquals(actual,expected);
+    }
+
+    @Test // Jucong test for ISOSCELES
+    public void test_isosceles_2() {
+      Type actual = Triangle.classify(2,1,2);
+      Type expected = ISOSCELES;
+      assertEquals(actual,expected);
+    }
+
+    @Test // Jucong test for ISOSCELES
+    public void test_isosceles_3() {
+      Type actual = Triangle.classify(2,2,1);
+      Type expected = ISOSCELES;
+      assertEquals(actual,expected);
+    }
+
+    @Test // Jucong test for ISOSCELES
+    public void test_isosceles_4() {
+      Type actual = Triangle.classify(2,2,100);
+      Type expected = INVALID;
+      assertEquals(actual,expected);
+    }
 }
